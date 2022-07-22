5078be12b820f2771077b5657fabf0f8ef53c4ea
spelar <spelar7@gmail.com>
2022-07-23 00:37:07 +0900

    back app.js update

--- back/app.js
+++ back/app.js
@@ -22,7 +22,7 @@ db.sequelize.sync()
 		console.log("db 연결 성공")
 	})
 	.catch(console.error);
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 passportConfig();
 
 app.use(morgan('dev'));
@@ -34,7 +34,7 @@ app.use(cors({
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.use(session({
   saveUninitialized: false,
   resave: false,
