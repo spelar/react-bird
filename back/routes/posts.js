a64dab05755b5254c75d2a725ba80f9783e7e966
spelar <spelar7@gmail.com>
2022-08-10 02:01:13 +0900

    back 팔로링한 게시글만 가져오도록

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -50,4 +50,122 @@ router.get('/', async (req, res, next) => {
 	}
 });
 
+router.get('/related', async (req, res, next) => {
+	try {
+		const followings = await User.findAll({
+			attributes: ['id'],
+			include: [{
+				model: User,
+				as: 'Followers',
+				where: {
+					id: req.user.id 
+				}
+			}]
+		});
+		
+		const where = {
+			UserId: { [Op.in]: [followings.map((v) => v.id)]}
+		};
+		if (parseInt(req.query.lastId, 10)) {
+			where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }
+		}
+		const posts = await Post.findAll({
+			where,
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC'],
+      ],
+      include: [{
+        model: User,
+        attributes: ['id', 'nickname'],
+      }, {
+        model: Image,
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }],
+      }, {
+        model: User, // 좋아요 누른 사람
+        as: 'Likers',
+        attributes: ['id'],
+      }, {
+        model: Post,
+        as: 'Retweet',
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }, {
+          model: Image,
+        }]
+      }],
+    });
+	res.status(200).json(posts);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
+router.get('/unrelated', async (req, res, next) => {
+	try {
+		const followings = await User.findAll({
+			attributes: ['id'],
+			include: [{
+				model: User,
+				as: 'Followers',
+				where: {
+					id: req.user.id 
+				}
+			}]
+		});
+		
+		const where = {
+			UserId: { [Op.notIn]: [followings.map((v) => v.id)]}
+		};
+		if (parseInt(req.query.lastId, 10)) {
+			where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }
+		}
+		const posts = await Post.findAll({
+			where,
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC'],
+      ],
+      include: [{
+        model: User,
+        attributes: ['id', 'nickname'],
+      }, {
+        model: Image,
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }],
+      }, {
+        model: User, // 좋아요 누른 사람
+        as: 'Likers',
+        attributes: ['id'],
+      }, {
+        model: Post,
+        as: 'Retweet',
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }, {
+          model: Image,
+        }]
+      }],
+    });
+	res.status(200).json(posts);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
 module.exports =  router;
